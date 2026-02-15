b3911da1987767b9efd167608693e7c5be0a409f
kr-sharath <krsharath16@gmail.com>
2026-02-15 14:23:51 +0530

    Feature/main (#12)
    * Changes added to test the workflow
    
    * Changes for github actions for checkout
    
    * Changes for github actions for checkout
    
    * Changes for github actions for checkout
    
    * Change in region for assume role
    
    * Added hashicorp setup to config file
    
    * echo directory
    
    * Changes to folder name
    
    * Change diretory
    
    * Change direcory Lambda to lammbda
    
    ---------
    
    Co-authored-by: Sharath <kr-sharath>

---
+++ modules/lambda/variables.tf
@@ -0,0 +1,44 @@
+variable "runtime" {
+  description = "The runtime of the lambda function"
+  default     = "nodejs20.x"
+  type        = string
+}
+
+variable "region" {
+  type    = string
+}
+
+variable "function_name" {
+  type        = string
+  description = "The name of the lambda function"
+}
+
+variable "lambda_role_name" {
+  type        = string
+  description = "The name of the IAM role for the lambda function"
+}
+
+variable "handler" {
+  type        = string
+  description = "value of the handler"
+}
+
+variable "policy_name" {
+  type        = string
+  description = "value of the policy name"
+}
+
+variable "timeout" {
+  type = number
+  description = "lambda execution time in seconds"
+}
+
+variable "s3_bucket" {
+  type = string
+  description = "name of the s3 bucket where code exists for lambda"
+}
+
+variable "s3_key" {
+  type = string
+  description = "name of the s3 key"
+}
