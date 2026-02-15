096082243fd691d4c8d1e4bef469110c7ab2170f
Sharath <kr-sharath>
2026-02-15 14:40:33 +0530

    change to backend config to remove use_lockfile argument

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "444198540026-test-bucket"
-    key          = "dev/terraform.tfstate"
+    key          = "terraform.tfstate"
     region       = "ap-south-1"
   }
 }
